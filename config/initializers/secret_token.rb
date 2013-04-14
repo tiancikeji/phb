dbad959f7e69fc6a54d704da211fed1079d0f832
王雷 <wanglei@chinacache.com>
2013-04-14 16:02:21 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phb::Application.config.secret_token = '8ab094f4d37cd1ad95f69949ecc556b116f188d447d7b0cda1076885b9e357e10f220606d171c5896657ad783a470304010e15a41866d16f942f3f75ea63d817'
