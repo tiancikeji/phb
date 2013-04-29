47b37c6738ead10e4d123c3aac59e20bfbb93453
王雷 <wanglei@chinacache.com>
2013-04-29 10:56:53 +0800

    update

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,6 @@ class Article < ActiveRecord::Base
   attr_accessible :content, :title, :user_id, :category_id, :summary, :cover, :from
   has_many :comments
   belongs_to :user
+  belongs_to :category
   mount_uploader :cover, ImageUploader
 end
