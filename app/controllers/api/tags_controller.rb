a129fd6f0b6ad88a059901119160331d3839bfc3
王雷 <wanglei@chinacache.com>
2013-05-30 18:50:08 +0800

    update

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,9 +1,17 @@
 class Api::TagsController < ApplicationController
 
   def show
-    @articles = Article.tagged_with(params[:id]).page params[:page]
-    @locations = Location.tagged_with(params[:id])
-    render :json => {:success=>true,:tag => params[:id],:articles => @articles,:locations => @locations}
+    articles = Article.tagged_with(params[:id]).page params[:page]
+    locations = Location.tagged_with(params[:id])
+    categories = {}
+    articles.each do |article|
+      # if categories.has_key? article.category.id
+        article_array = []
+        article_array.append(article) 
+        categories.store(article.category.name,article_array)
+      # end
+    end
+    render :json => {:success=>true,:tag => params[:id],:categories => categories,:locations => locations}
   end
 
 end
