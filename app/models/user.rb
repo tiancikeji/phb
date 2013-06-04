1cc937e6fe5045086d62447d09e7cee29d23aefb
王雷 <wanglei@chinacache.com>
2013-06-04 23:54:00 +0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :note
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :note, :nickname, :gender
   has_many :articles  
   has_many :comments
   has_many :babycards
